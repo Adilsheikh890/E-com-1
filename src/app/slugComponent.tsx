95f4220addd055f1ba3a0305f142ca8efbe10682
Adil sheikh <sheikhadil044@gmail.com>
2023-08-12 14:39:54 -0700

    new

---
+++ src/app/slugComponent.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Image from 'next/image'
+
+const  SlugComponent=() =>{
+  return (
+    <div>
+           <Image src="/pu.webp" alt="image" width={400} height={400} />
+           <div>
+            
+           </div>
+    </div>
+  )
+}
+
+export default SlugComponent
